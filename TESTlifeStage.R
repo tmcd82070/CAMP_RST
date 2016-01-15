50a74a99a1ce49356c16469dfc7b4509c9a5d73a
studyvin <jstudyvin@west-inc.com>
2016-01-15 10:36:57 -0700

    pull data from mbd
    The function to query the database seems to be working

--- TESTlifeStage.R
+++ TESTlifeStage.R
@@ -3,40 +3,32 @@
 ## 11 Jan 2016
 ## PSMFS 469-07.005 phase 4
 ## PM: Trent
-## Query the data base to get the data for life stage assignment
+## This is my test script for assigning the life stage
 #####################################
 
+rm(list=ls())
 
-
-
-
-taxon <- '161980'           # always chinook salmon
-site <- '42000'             # this is the river -- this is in table 'site' in the mdbs.
-min.date <- '1990-01-01'    # the pre queries need to restrict to a time frame --
-max.date <- '2015-12-31'    #  some data goes back to the 90s
-
-
-taxon='161980'
-site='42000'
-min.date='1990-01-01'
-max.date='2015-12-31'
-
-
-##pathData <- "//lar-file-srv/Data/PSMFC_CampRST/ThePlatform/CAMP_RST20151130/Data/TestingDBs/CAMP_RBDD_29Jan2015"     # i point to an mdb
-
-
-pathData <- 'C:/Users/jstudyvin/GoogleDrive/project/PSMFS/fishLength/CAMP_RST20151014-DougEagleman/Data/TestingDBs/CAMP_RBDD_29Jan2015/'
+library(plyr)
 
 
 codePath <- '~/GoogleDrive/project/PSMFS/fishLength/CAMP_RST/'
-setwd(codePath)
-## point somewhere / somehow to where the queries are
 
-source( paste0(codePath,"/run_sqlFile.r" ))# a trent f'n from the platform
-source( paste0(codePath,"/sql_error_check.r" ))# a trent f'n from the platform
-source( paste0(codePath,"/build_Report_Criteria.r" ))# a trent f'n from the platform
-source( paste0(codePath,"/getDataForLifeStageAssign.R" ))# a trent f'n from the platform
+load(paste0(codePath,'catchData.Rdata'))
 
 
 
+head(catchData)
 
+
+catchData$whereWeight <- !is.na(catchData$weight)
+nrow(catchData)
+with(catchData,sum(Unmarked==0))
+with(catchData,unique(Unmarked))
+with(catchData,table(Unmarked,whereWeight,useNA='ifany'))
+with(catchData,table(river,FinalRun,whereWeight,useNA='ifany'))
+
+
+out <- ddply(catchData,~river+site,summarize,missingWeight=sum(is.na(weight)),haveWeight=sum(!is.na(weight)),n=length(weight),um=sum(Unmarked>1))
+out
+
+out$missingWeight+out$haveWeight
--- getDataForLifeStageAssign.R
+++ getDataForLifeStageAssign.R
@@ -6,13 +6,22 @@
 
 
 
-getDataForLifeStageAssign <- function(pathData,taxon='161980',site='42000',min.date='1990-01-01',max.date='2015-12-31'){
+getDataForLifeStageAssign <- function(pathData,taxon,site,min.date,max.date){
+
+print('Data from:')
+print(tail(strsplit(pathData,'/')[[1]],1))
+
 
 require(RODBC)
 
 ## the string must be 'db.file', or else f'n 'build_Report_Criteria.r' won't work.
-db.file <<- paste0(pathData,"CAMP.mdb")
-file.exists(db.file)
+db.file <<- paste0(pathData,"/CAMP.mdb")
+
+if(!file.exists(db.file)){
+    print('file does not exist:')
+    print(db.file)
+    stop()
+}
 
 # ---- build the other queries connie mentions in her new sql query ----
 nvisits <- F.buildReportCriteria( site, min.date, max.date )      # trent f'n to build first report in a query sequence
@@ -22,11 +31,20 @@ if( nvisits == 0 ){
   return()
 }
 
+print('Data from:')
+print(tail(strsplit(pathData,'/')[[1]],1))
+
+
 db <- get( "db.file", env=.GlobalEnv )
 ch <- odbcConnectAccess(db)
 
 F.run.sqlFile(ch, "QrySamplePeriod.sql", R.TAXON=taxon ) # trent f'n that works as an access-sql handler
 ## ---- end building ----
+
+
+print('Data from:')
+print(tail(strsplit(pathData,'/')[[1]],1))
+
 
 ## ---- finally, the new query ----
 F.run.sqlFile(ch,'QryUnmarkedChinookLifeStages.sql',TRUE,FALSE,R.TAXON=taxon)
@@ -36,7 +54,12 @@ F.sql.error.check(catch)   # trent function to make sure the table seems ok
 close(ch) # disconnect
 ## ---- end new query -----
 
+print('Data from:')
+print(tail(strsplit(pathData,'/')[[1]],1))
+
 ## return the data
 return(catch)
+
+
 
 } ## function
