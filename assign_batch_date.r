2b4f6fcf217ee763b788f1a353722089aebdb07d
Jason <jmitchell@west-inc.com>
2016-04-07 13:31:10 -0600

    Comments for the help file functionality for a few different functions; i.e., assign_batch_date.r, summarize_index.r, summarize_passage.r.

--- assign_batch_date.r
+++ assign_batch_date.r
@@ -1,59 +1,119 @@
+#' @title Assign batch date to records.
+#'   
+#' @param df The data frame containing temporal records for which a day must be 
+#'   assigned.
+#'   
+#' @return Function \code{F.assign.batch.date} maps trapping records containing 
+#'   trapping-instance variable \code{EndTime}s to particular days, taking into
+#'   consideration that trapping instances often take place over midnight, i.e.,
+#'   two separate calendar days.  This function also considers multi-day
+#'   trapping instances.
+#'   
+#'   The function works by utilizing a standardized time, via which trapping 
+#'   periods are partitioned into particular days.  The cut time utilized to 
+#'   assign minutes to a particular day is 4:00 AM Pacific Time (regardless of 
+#'   Daylight Savings).  As of release 4.5, this value is set via global 
+#'   variable \code{samplePeriodCutTime}.  The time of 4:00 AM was chosen, 
+#'   rather than midnight, in order to not prematurely cut-off trapping 
+#'   instances that often continue past midnight.
+#'   
+#'   Generally, the variable created by function \code{F.assign.batch.date}, 
+#'   \code{batchDate}, is the same as variable \code{SampleDate} found in 
+#'   query-derived table \code{TempSumUnmarkedByTrap_Run_Final} in any CAMP 
+#'   Database.  Variable \code{batchDate} replaces variable \code{SampleDate} in
+#'   all dataframes for which function \code{F.assign.batch.date} is called.  To
+#'   avoid confusion, new variable \code{batchDate} replaces old variable 
+#'   \code{SampleDate} after the running of this function.
+#'   
+#' @section: Details: In order to process the sequence of dates correctly, 
+#' function \code{F.assign.batch.date} first identifies the earliest and latest 
+#' dates in the provided data frame's trapping \code{EndTime} variable.  Given 
+#' \code{EndTime} dates and times, it then moves back one day in time, and two 
+#' forward; this encapsulates the earliest and latest \code{EndTime} dates. 
+#' Given the unique dates within this resulting range, each of which start at 
+#' 12:00 midnight, it then adds the value of global variable 
+#' \code{samplePeriodCutTime};  this effectively moves the traditional 
+#' day-separation time of midnight to the value of \code{samplePeriodCutTime}, 
+#' or 4:00 AM as of release 4.5, for the purposes of assigning fish-counts to
+#' particular days.  See Examples.  
+#' 
+#' All calculations consider daylight savings time and assume that measurements 
+#' take place within the Pacific Time Zone.
+#' 
+#' @seealso
+#' 
+#' @aliases
+#' 
+#' @examples
+#' # Set the time zone to Pacific.
+#' time.zone <- "America/Los_Angeles"
+#' 
+#' # Create a data set of three trapping instances.  
+#' origDate <- data.frame(trapVisitID=c(1234,1235),
+#' SampleDate=strptime(c("2014-01-24","2014-01-24"),"%F"),
+#' StartTime=strptime(c("2014-01-23 14:28:00","2014-01-24 03:59:00"),"%Y-%m-%d %H:%M:%S", tz=time.zone),
+#' EndTime=strptime(c("2014-01-24 03:59:00","2014-01-24 04:00:00"),"%Y-%m-%d %H:%M:%S", tz=time.zone))
+#'
+#' # BatchDate for \code{trapVisitID=1234} is now 1/23, due to that trap stopping before 4:00 AM.  
+#' newDate <- F.assign.batch.date(origDate)
+#' @export
+
 F.assign.batch.date <- function( df ){
-#
-#   Assign batch data to records.
-#
-# df <- catch                      # df <- nvCatch2
 
-cuttime <- get( "samplePeriodCutTime", env=.GlobalEnv )
-midtime <- "00:00:00"   # this is the time of day assigned to batchDates.  Could be half way between cut times or (cuttime - 12*60*60).
+  
+  
+#   startTime <- strptime("2014-01-24 03:55:00","%Y-%m-%d %H:%M:%S",tz=time.zone)
+#   times <- data.frame(EndTime=seq(from=startTime,by=60,length.out=180))
+  
+  
+  
+  # df <- times     df <- origDate[,c('EndTime')]              # df <- nvCatch2
 
-time.zone <- get( "time.zone", env=.GlobalEnv )
+  cuttime <- get( "samplePeriodCutTime", env=.GlobalEnv )
+  midtime <- "00:00:00"   # this is the time of day assigned to batchDates.  Could be half way between cut times or (cuttime - 12*60*60).
+  time.zone <- get( "time.zone", env=.GlobalEnv )
 
+  #   A sequence of dates at cuttime every day
+  #min.day <- as.POSIXlt( min(df$EndTime) - 24*60*60, format="%Y-%m-%d %H:%M:%S", tz=time.zone)
+  #max.day <- as.POSIXlt( max(df$EndTime) + 2*24*60*60, format="%Y-%m-%d %H:%M:%S", tz=time.zone)
+  min.day <- min(df$EndTime) - 24*60*60
+  max.day <- max(df$EndTime) + 2*24*60*60
+  cut.seq <- seq( min.day, max.day, by=24*60*60 )
+  cut.day <- format( cut.seq, "%Y-%m-%d" )
+  cut.seq <- unique(as.POSIXct( paste( cut.day, cuttime ), format="%Y-%m-%d %H:%M:%S", tz=time.zone))
 
-#   A sequence of dates at cuttime every day
-#min.day <- as.POSIXlt( min(df$EndTime) - 24*60*60, format="%Y-%m-%d %H:%M:%S", tz=time.zone)
-#max.day <- as.POSIXlt( max(df$EndTime) + 2*24*60*60, format="%Y-%m-%d %H:%M:%S", tz=time.zone)
-min.day <- min(df$EndTime) - 24*60*60
-max.day <- max(df$EndTime) + 2*24*60*60
-cut.seq <- seq( min.day, max.day, by=24*60*60 )
-cut.day <- format( cut.seq, "%Y-%m-%d" )
-cut.seq <- unique(as.POSIXct( paste( cut.day, cuttime ), format="%Y-%m-%d %H:%M:%S", tz=time.zone))
+  #   Bin the sampleEnd's to cut.seq
+  ind <- cut( df$EndTime, cut.seq, labels=FALSE )
 
+  #   Establish when the cut time "wraps" to the next day.  I.e., at some point, as cut time increases from 0, you
+  #   stop calling the sample from the night before, and attribute it to the current night.  This time is set in 
+  #   wrap.POSIX.  jason 4/7/2016 -- note that cut.POSIX < wrap.POSIX always (because we set cuttime to be 4 AM), 
+  #   so not sure why this is here.  maybe an artifact from a much older version?  
+  cut.POSIX <- as.POSIXct( paste("1970-1-1", cuttime, format="%Y-%m-%d %H:%M:%S" ))
+  wrap.POSIX <- as.POSIXct("1970-1-1 06:00:00", format="%Y-%m-%d %H:%M:%S" )
 
-#   Bin the sampleEnd's to cut.seq
-ind <- cut( df$EndTime, cut.seq, labels=FALSE )
-
-
-#   Establish when the cut time "wraps" to the next day.  I.e., at some point, as cut time increases from 0, you
-#   stop calling the sample from the night before, and attribute it to the current night.  This time is set in 
-#   wrap.POSIX.
-cut.POSIX <- as.POSIXct( paste("1970-1-1", cuttime, format="%Y-%m-%d %H:%M:%S" ))
-wrap.POSIX <- as.POSIXct("1970-1-1 06:00:00", format="%Y-%m-%d %H:%M:%S" )
-
- 
-if( cut.POSIX < wrap.POSIX ){
-    add.day <- 0
-} else {
-    add.day <- 1
-}
+  if( cut.POSIX < wrap.POSIX ){
+      add.day <- 0
+  } else {
+      add.day <- 1
+  }
     
-#   Compute batchDate    
-bDate <- as.POSIXct( paste( format(cut.seq[ind] + add.day*24*60*60, "%Y-%m-%d"), midtime), format="%Y-%m-%d %H:%M:%S", tz=time.zone)
+  #   Compute batchDate    
+  bDate <- as.POSIXct( paste( format(cut.seq[ind] + add.day*24*60*60, "%Y-%m-%d"), midtime), format="%Y-%m-%d %H:%M:%S", tz=time.zone)
 
-
-##   If we allow biologists to over ride batchDate, uncomment the following code
-##   Figure out which batchDates are missing and replace just those.
-#miss <- is.na(df$batchDate)
-#df$batchDate[miss] <- bDate[miss]
-
-#   Add batch date to data frame
-if( "SampleDate" %in% names(df) ){
+  #   If we allow biologists to override batchDate, uncomment the following code.
+  #   Figure out which batchDates are missing and replace just those.
+  #miss <- is.na(df$batchDate)
+  #df$batchDate[miss] <- bDate[miss]
+  
+  #   Add batch date to data frame
+  if( "SampleDate" %in% names(df) ){
     #   overwrite with batchDate
     df$SampleDate <- bDate
     names(df)[names(df) == "SampleDate" ] <- "batchDate"
-} else {
+  } else {
     df$batchDate <- bDate
-}
+  }
 
-df
+  df
 }
--- summarize_index.r
+++ summarize_index.r
@@ -1,3 +1,64 @@
+#' @title Average passage over all traps at a site, then sum by summarize.by.
+#'   
+#' @param df The data frame containing days which are to be summarized via  
+#'   the temporal unit specified by \code{summarize.by}.
+#' @param summarize.by The time unit into which summary occurs, i.e., one of 
+#' \code{day},\code{week},\code{month},\code{year}.
+#' 
+#' @return The dataframe fed to function \code{F.summarize.passage} must have a 
+#'   column \code{batchDate}, including the day fished, and a column 
+#'   \code{imputed.catch}, containing the percentage of the total count of the
+#'   Total Estimated Catch obtained via imputation.  Additionally, the 
+#'   dataframe must contain estimated passage.  See Details.  
+#'   
+#'   Function \code{F.summarize.passage} first averages the passage on a per-day basis 
+#'   over all traps running on that day.  It then summarizes via the temporal units 
+#'   specified by \code{summarize.by}. See Examples.
+#'   
+#'   By default, passage estimation involving lifestage always takes place over 
+#'   a year.
+#'   
+#' @section: Details:  The Total Estimated Catch is the sum of Assigned,
+#' Unassigned, Half-Cone Adjusted, and Imputed Fish.  It is also the numerator
+#' utilized in the estimation of passage, where the denominator is the estimated
+#' efficiency.
+
+#' @seealso
+#' 
+#' @aliases
+#' 
+#' @examples
+#' 
+#' # Fetch Julian week labels from a local CAMP.mdb.
+#' # These are necessary for summaries by week.  
+#' db.file <- "C:/CAMP Folder/CAMP.mdb"                       
+#' db <- get( "db.file", env=.GlobalEnv )
+#' ch <- odbcConnectAccess(db)
+#' the.dates <- sqlFetch( ch, "Dates" )
+#' close(ch) 
+#'  
+#' # Create a data set of trapping.  
+#' beg <- strptime("2014-01-24",format="%F")
+#' df <- data.frame(trapVisitID=c(seq(1234,length.out=10)),
+#'                  trapPositionID=c(rep(10001,10),rep(10002,10)),
+#'                  batchDate=rep(seq(beg,by=60*60*24,length.out=10),2),
+#'                  imputed.catch=c(0,0,0,0.1,0,0,1.0,1.0,0,0,0,0,0,0.1,0,0,0,0,0,1),
+#'                  passage=c(11,11,14,16,12,10,23,18,17,18,3,5,2,7,5,4,1,3,1,1))
+#' df <- df[order(df$batchDate,df$trapPositionID),]
+#' 
+#' # Summarize by different time frames. 
+#' df.day <- F.summarize.passage( df, "day" )
+#' df.week <- F.summarize.passage( df, "week" )
+#' df.month <- F.summarize.passage( df, "month" )
+#' df.year <- F.summarize.passage( df, "year" )
+#' 
+#' @export
+
+
+
+
+
+
 F.summarize.index <- function( dt, summarize.by ){
 #
 #   return a list suitable for calling tapply which summarizes a vector by 'summarize.by'.
@@ -8,74 +69,25 @@ F.summarize.index <- function( dt, summarize.by ){
 # dt <- catch.df$batchDate
 
 
-f.week <- function( x ){
-#     #   A utility to compute julian weeks 
-#     #   x = vector of POSIXct's
-# 
-#     yr <- format( x, "%Y" )
-# 
-# #     jday <- as.numeric(format( x, "%j" ))   # for non-leap years, the count doesnt skip a day on 2/29.  we need it to do so for consistency over years.
-#     
-#     # ------------------------------------------------
-#     myYear = as.POSIXlt(x)$year + 1900
-# 
-#     leap <- rep(NA,length(myYear))
-#     for(i in 1:length(myYear)){
-#       if(myYear[i] %% 4 != 0){                         # wikipedia article on leap year.
-#         leap[i] <- 0
-#       } else if (myYear[i] %% 100 != 0){
-#         leap[i] <- 1
-#       } else if (myYear[i] %% 400 != 0){
-#         leap[i] <- 0
-#       } else {
-#         leap[i] <- 1
-#       }
-#     }
-# 
-#     tmp.jday <- rep(NA,length(myYear))
-#     for(i in 1:length(myYear)){
-#       if(leap[i] == 1){  # leap year -- feb 29th included.
-#         tmp.jday[i] <- as.numeric(format(x[i], "%j"))
-#       } else {           # not a leap year -- feb 29th not included. adjust.
-#         if(as.numeric(format(x[i], "%j")) >= 60 ){            # 60 = feb 29th on leap years. 61 = mar 1st on leap years.
-#           tmp.jday[i] <- as.numeric(format(x[i], "%j")) + 1     # this pushes march 1st for non-leap to day 61 instead of day 60.                 
-#         } else {
-#           tmp.jday[i] <- as.numeric(format(x[i], "%j"))
-#         }
-#       }
-#     }
-#     # ------------------------------------------------
-# 
-#     wk <- trunc((tmp.jday - 1) / 7) + 1
-#     wk <- formatC(wk, width=2, flag="0")  # to make sure sort order is correct
-#     
-#     wk <- paste(yr, "-", wk, sep="")
-#     wk
+  f.week <- function( x ){
+    the.dates$week <- paste0(the.dates$year,'-',formatC(the.dates$julianWeek, width=2, flag="0"))
+    the.dates <- unique(the.dates)
+    the.dates$uniqueDate <- as.Date(the.dates$uniqueDate)
   
-  # possibly obsolete.
-#   db <- get( "db.file", env=.GlobalEnv )                                  #   Open ODBC channel
-#   ch <- odbcConnectAccess(db)
-#   the.dates <- sqlFetch( ch, "Dates" )                                    #   get the table that has the julian week labels.
-#   the.dates <- subset(the.dates, as.Date(uniqueDate) >= min.date & as.Date(uniqueDate) <= max.date,c(uniqueDate,year,julianWeek,julianWeekLabel))
-#   close(ch)
-  the.dates$week <- paste0(the.dates$year,'-',formatC(the.dates$julianWeek, width=2, flag="0"))
-  the.dates <- unique(the.dates)
-  the.dates$uniqueDate <- as.Date(the.dates$uniqueDate)
-  
-  dtDF <- data.frame(uniqueDate=as.Date(dt))
-  dtDF$R_ID <- seq(1,nrow(dtDF),1)
-  test <- merge(dtDF,the.dates,by=c('uniqueDate'),all.x=TRUE)
-  test <- test[order(test$R_ID),]
-  ans <- test$week
-  ans
-}
+    dtDF <- data.frame(uniqueDate=as.Date(dt))
+    dtDF$R_ID <- seq(1,nrow(dtDF),1)
+    test <- merge(dtDF,the.dates,by=c('uniqueDate'),all.x=TRUE)
+    test <- test[order(test$R_ID),]
+    ans <- test$week
+    ans
+  }
 
-#   ---- Construct index
-if( summarize.by == "week" ){
+  #   ---- Construct index
+  if( summarize.by == "week" ){
     index <- list(s.by=f.week( dt ) )
-} else if( summarize.by == "month" ){
+  } else if( summarize.by == "month" ){
     index <- list(s.by= format( dt, "%Y-%m" ))
-} else if( summarize.by == "year" ){
+  } else if( summarize.by == "year" ){
     #   Because we checked, and min.date and max.date are less than 365 days appart, for annual numbers
     #   we just sum everything.  This is necessary because most winter runs occur in two calender years.
     year.of.mean.date <- mean( dt, na.rm=T )
@@ -84,11 +96,9 @@ if( summarize.by == "week" ){
     year.of.mean.date <- as.POSIXct( year.of.mean.date-tz.offset, origin="1970-01-01", tz=tzn )  # I think this only works west of GMT (North America).
     year.of.mean.date <- format(year.of.mean.date, "%Y")
     index <- list(s.by= rep( year.of.mean.date, length(dt) ))
-} else { # summarize by day.  
+  } else { # summarize by day.  
     index <- list(s.by= format( dt, "%Y-%m-%d" ))
-}
+  }
 
-
-index
-
+  index
 }
