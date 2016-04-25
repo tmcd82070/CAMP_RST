713ab376c9c38a73919a2435ccd56c1f82e519d4
Jason <jmitchell@west-inc.com>
2016-04-25 09:41:44 -0600

    Trying to get all this worked out.

--- assignLSCompare.R
+++ assignLSCompare.R
@@ -7,208 +7,210 @@
 
 
 assignLSCompare <- function(Data,SAVE=TRUE){
-
-    ## keep only needed columns
-    Data <- Data[,c('days','lifeStage','SampleDate','FinalRun','forkLength','weight','Unmarked','bioLS')]
-
-
-    ## get needed packages
-    getPackages(c('plyr','ellipse','tidyr'))
-
-    ## order the levels of the life stage
-    (LS <- as.character(unique(Data[,'lifeStage'])))
-    (lvl <- c(LS[grepl('small',LS,ignore.case=TRUE)],
-             LS[grepl('med',LS,ignore.case=TRUE)],
-             LS[grepl('Large',LS,ignore.case=TRUE)],
-             LS[grepl('^all',LS,ignore.case=TRUE)],
-             LS[grepl('^unass',LS,ignore.case=TRUE)],
-              LS[grepl('^fail',LS,ignore.case=TRUE)]))
-    Data[,'lifeStage'] <- factor(Data[,'lifeStage'],levels=lvl,ordered=TRUE)
-
-
-    ## for debugging
-    ##data <- subset(Data,FinalRun=='Fall')
-
-    compare <- function(data,save){
-
-        ## this run
-        (fRun <- as.character(data[1,'FinalRun']))
-
-        if(grepl('unass',fRun,ignore.case=TRUE)){
-            return(NULL)
-        }
-
-        (LSlvl <- as.character(unique(data[,'lifeStage'])))
-
-        if(any(grepl('fail',LSlvl,ignore.case=TRUE))){
-            return(NULL)
-        }
-
-
-
-        if(sum(!grepl('unass',LSlvl,ignore.case=TRUE))==0){
-            return(NULL)
-        }
-
-
-
-        cat('\n')
-        cat('\n')
-        cat('\n')
-        cat('Generating comparison results for run =',fRun,'\n')
-
-
-        ## confusion matrix
-        compareDF <- ddply(data,~bioLS+lifeStage,summarize,fish=sum(Unmarked))
-        cvTab <- spread(compareDF,key=lifeStage,value=fish,fill=0)
-
-        cat('Confusion Matrix \n')
-        print(cvTab)
-
-        if(save){
-            write.csv(cvTab,paste0(output.file,gsub(' ','',fRun),'ConfusionMatrix.csv'),row.names=FALSE)
-        }
-        ##head(data)
-
-        ## mixture distribution life stage level colors
-        mixIndex <- data.frame(lifeStage=c('Small','Medium','Large','All'),col=c('red','green','blue','orange'),stringsAsFactors=FALSE)
-
-        ## biologist life stage symbols
-        bioIndex <- data.frame(bioLS=c('Fry','Parr','Smolt'),pch=1:3,stringsAsFactors=FALSE)
-
-        ## data to be plotted now
-        data <- merge(merge(data,mixIndex,all.x=TRUE),bioIndex,all.x=TRUE)
-
-        ##nrow(plotData)
-        ##nrow(data)
-        ##head(plotData)
-
-        ## add ellipse to the figure
-        addEllipse <- function(run){
-            ## add ellipse to the figure
-
-            vars <- c('days','forkLength')
-
-            mu <- mixDistMUList[[run]]
-            if(is.na(mu)){
-                return(NULL)
-            }
-
-            Sigma <- mixDistSigmaList[[run]]
-            for(j in 1:ncol(mu)){
-                points(ellipse::ellipse(Sigma[vars,vars,j],centre=mu[vars,j]),type='l')
-
-            } # end for j
-
-            return(NULL)
-
-        } # end addEllipse function
-
-       ## save.image(file="C:/Users/jmitchell/Desktop/FirstLineBigLooper.RData")
-
-        monthLabel <- data.frame(month.abb,first=c('01-01','02-01','03-01','04-01','05-01','06-01','07-01','08-01','09-01','10-01','11-01','12-01'),stringsAsFactors=FALSE)
-
-        for(i in 1:nrow(monthLabel)){
-            j <- 1;goodMonth <- FALSE
-
-            while(!goodMonth){
-                firstDay <- with(data,mean(days[format(SampleDate,'%m-%d')==paste0(formatC(i,width=2,flag=0),'-',formatC(j,width=2,flag=0))]))
-
-                if(!is.na(firstDay)){
-                    monthLabel[i,'days'] <- firstDay-(j-1)
-                    goodMonth <- TRUE
-                }else if(j>31){
-                    goodMonth <- TRUE
-                    monthLabel[i,'days'] <- NA
-                }else{
-                    j <- j+1
-                }
-
-            } # end while
-        } # end for i
-
-
-        ##monthLabel
-
-
-
-        cat('\n')
-        cat('\n')
-        cat('\n')
-        cat('Saving comparison figure.\n')
-        if(save){
-            pdf(file=paste0(output.file,gsub(' ','',fRun),'plotLifeStageAssignComparison.pdf'),width=7)
-        }else{
-            windows(width=7)
-        }
-        ##par(mfrow=c(1,2))
-
-        ## title for figure
-        varUsed <- paste(rownames(mixDistMUList[[fRun]]),collapse=", ")
-        plotMain <- paste0(fRun,'\nVariables used to assign lifestage: ',gsub('days','date',varUsed))
-        ## plot forklength and date
-        with(data,plot(days,forkLength,ylab='Fork Length (mm)',xlab='Sample Date',col=col,pch=pch,xaxt='n',main=plotMain))
-        addEllipse(fRun)
-        with(monthLabel,axis(1,at=days,label=month.abb))
-
-
-        ##with(data,table(col,pch))
-
-
-        ## legend info for mixture life stage
-        legMix <- mixIndex[mixIndex$lifeStage%in%LSlvl[!grepl('^unass',LSlvl,ignore.case=TRUE)],]
-        legMix$pch <- 20
-
-        ## legend info for biologist life stage
-        biolvl <- as.character(unique(data[,'bioLS']))
-        havebiolvl <- c(biolvl[grepl('^fry',biolvl,ignore.case=TRUE)],
-        biolvl[grepl('^parr',biolvl,ignore.case=TRUE)],
-        biolvl[grepl('^smolt',biolvl,ignore.case=TRUE)])
-        legBio <- bioIndex[bioIndex$bioLS%in%havebiolvl,]
-        names(legBio) <- c('lifeStage','pch')
-        legBio$col <- 'black'
-        legBio <- legBio[,c('lifeStage','col','pch')]
-        ##legMix
-        ##legBio
-
-
-        ## prepares the legend so the color and pch match up correctly
-        d <- nrow(legMix) - nrow(legBio)
-        if(d==0){
-
-            legAll <- rbind(legMix,legBio)
-
-        }else if(d>0){
-            addRow <- as.data.frame(matrix(NA,nrow=d,ncol=ncol(legMix)))
-            names(addRow) <- names(legMix)
-            legAll <- rbind(legMix,legBio,addRow)
-        }else if(d<0){
-            addRow <- as.data.frame(matrix(NA,nrow=abs(d),ncol=ncol(legMix)))
-            names(addRow) <- names(legMix)
-            legAll <- rbind(legMix,addRow,legBio)
-        }
-        legAll
-
-
-        with(legAll,legend('topleft',legend=lifeStage,col=col,pch=pch,ncol=2,bg='white'))
-
-
-        if(save){
-            graphics.off()
-        }
-
+  
+  ## keep only needed columns
+  Data <- Data[,c('days','lifeStage','SampleDate','FinalRun','forkLength','weight','Unmarked','bioLS')]
+  
+  
+  ## get needed packages
+  getPackages(c('plyr','ellipse','tidyr'))
+  
+  ## order the levels of the life stage
+  (LS <- as.character(unique(Data[,'lifeStage'])))
+  (lvl <- c(LS[grepl('small',LS,ignore.case=TRUE)],
+            LS[grepl('med',LS,ignore.case=TRUE)],
+            LS[grepl('Large',LS,ignore.case=TRUE)],
+            LS[grepl('^all',LS,ignore.case=TRUE)],
+            LS[grepl('^unass',LS,ignore.case=TRUE)],
+            LS[grepl('^fail',LS,ignore.case=TRUE)]))
+  Data[,'lifeStage'] <- factor(Data[,'lifeStage'],levels=lvl,ordered=TRUE)
+  
+  
+  ## for debugging
+  ##data <- subset(Data,FinalRun=='Fall')
+  
+  compare <- function(data,save){
+    
+    ## this run
+    (fRun <- as.character(data[1,'FinalRun']))
+    
+    if(grepl('unass',fRun,ignore.case=TRUE)){
+      return(NULL)
+    }
+    
+    (LSlvl <- as.character(unique(data[,'lifeStage'])))
+    
+    if(any(grepl('fail',LSlvl,ignore.case=TRUE))){
+      return(NULL)
+    }
+    
+    
+    
+    if(sum(!grepl('unass',LSlvl,ignore.case=TRUE))==0){
+      return(NULL)
+    }
+    
+    
+    
+    cat('\n')
+    cat('\n')
+    cat('\n')
+    cat('Generating comparison results for run =',fRun,'\n')
+    
+    
+    ## confusion matrix
+    compareDF <- ddply(data,~bioLS+lifeStage,summarize,fish=sum(Unmarked))
+    cvTab <- spread(compareDF,key=lifeStage,value=fish,fill=0)
+    
+    cat('Confusion Matrix \n')
+    print(cvTab)
+    
+    if(save){
+      write.csv(cvTab,paste0(output.file,gsub(' ','',fRun),'ConfusionMatrix.csv'),row.names=FALSE)
+    }
+    ##head(data)
+    
+    ## mixture distribution life stage level colors
+    mixIndex <- data.frame(lifeStage=c('Small','Medium','Large','All'),col=c('red','green','blue','orange'),stringsAsFactors=FALSE)
+    
+    ## biologist life stage symbols
+    bioIndex <- data.frame(bioLS=c('Fry','Parr','Smolt'),pch=1:3,stringsAsFactors=FALSE)
+    
+    ## data to be plotted now
+    data <- merge(merge(data,mixIndex,all.x=TRUE),bioIndex,all.x=TRUE)
+    
+    ##nrow(plotData)
+    ##nrow(data)
+    ##head(plotData)
+    
+    ## add ellipse to the figure
+    addEllipse <- function(run){
+      ## add ellipse to the figure
+      
+      vars <- c('days','forkLength')
+      
+      mu <- mixDistMUList[[run]]
+      if(is.na(mu)){
         return(NULL)
-    }# end compare function
-
-    ## for debugging
-    ##ddply(Data,~FinalRun,compare,save=FALSE)
-
-    ddply(Data,~FinalRun,compare,save=SAVE)
-
+      }
+      
+      Sigma <- mixDistSigmaList[[run]]
+      for(j in 1:ncol(mu)){
+        points(ellipse::ellipse(Sigma[vars,vars,j],centre=mu[vars,j]),type='l')
+        
+      } # end for j
+      
+      return(NULL)
+      
+    } # end addEllipse function
+    
+    
+    ## save.image(file="C:/Users/jmitchell/Desktop/FirstLineBigLooper.RData")
+    
+    
+    monthLabel <- data.frame(month.abb,first=c('01-01','02-01','03-01','04-01','05-01','06-01','07-01','08-01','09-01','10-01','11-01','12-01'),stringsAsFactors=FALSE)
+    
+    for(i in 1:nrow(monthLabel)){
+      j <- 1;goodMonth <- FALSE
+      
+      while(!goodMonth){
+        firstDay <- with(data,mean(days[format(SampleDate,'%m-%d')==paste0(formatC(i,width=2,flag=0),'-',formatC(j,width=2,flag=0))]))
+        
+        if(!is.na(firstDay)){
+          monthLabel[i,'days'] <- firstDay-(j-1)
+          goodMonth <- TRUE
+        }else if(j>31){
+          goodMonth <- TRUE
+          monthLabel[i,'days'] <- NA
+        }else{
+          j <- j+1
+        }
+        
+      } # end while
+    } # end for i
+    
+    
+    ##monthLabel
+    
+    
+    
     cat('\n')
     cat('\n')
     cat('\n')
-    cat('End assignLSCompare function.\n')
-
+    cat('Saving comparison figure.\n')
+    if(save){
+      pdf(file=paste0(output.file,gsub(' ','',fRun),'plotLifeStageAssignComparison.pdf'),width=7)
+    }else{
+      windows(width=7)
+    }
+    ##par(mfrow=c(1,2))
+    
+    ## title for figure
+    varUsed <- paste(rownames(mixDistMUList[[fRun]]),collapse=", ")
+    plotMain <- paste0(fRun,'\nVariables used to assign lifestage: ',gsub('days','date',varUsed))
+    ## plot forklength and date
+    with(data,plot(days,forkLength,ylab='Fork Length (mm)',xlab='Sample Date',col=col,pch=pch,xaxt='n',main=plotMain))
+    addEllipse(fRun)
+    with(monthLabel,axis(1,at=days,label=month.abb))
+    
+    
+    ##with(data,table(col,pch))
+    
+    
+    ## legend info for mixture life stage
+    legMix <- mixIndex[mixIndex$lifeStage%in%LSlvl[!grepl('^unass',LSlvl,ignore.case=TRUE)],]
+    legMix$pch <- 20
+    
+    ## legend info for biologist life stage
+    biolvl <- as.character(unique(data[,'bioLS']))
+    havebiolvl <- c(biolvl[grepl('^fry',biolvl,ignore.case=TRUE)],
+                    biolvl[grepl('^parr',biolvl,ignore.case=TRUE)],
+                    biolvl[grepl('^smolt',biolvl,ignore.case=TRUE)])
+    legBio <- bioIndex[bioIndex$bioLS%in%havebiolvl,]
+    names(legBio) <- c('lifeStage','pch')
+    legBio$col <- 'black'
+    legBio <- legBio[,c('lifeStage','col','pch')]
+    ##legMix
+    ##legBio
+    
+    
+    ## prepares the legend so the color and pch match up correctly
+    d <- nrow(legMix) - nrow(legBio)
+    if(d==0){
+      
+      legAll <- rbind(legMix,legBio)
+      
+    }else if(d>0){
+      addRow <- as.data.frame(matrix(NA,nrow=d,ncol=ncol(legMix)))
+      names(addRow) <- names(legMix)
+      legAll <- rbind(legMix,legBio,addRow)
+    }else if(d<0){
+      addRow <- as.data.frame(matrix(NA,nrow=abs(d),ncol=ncol(legMix)))
+      names(addRow) <- names(legMix)
+      legAll <- rbind(legMix,addRow,legBio)
+    }
+    legAll
+    
+    
+    with(legAll,legend('topleft',legend=lifeStage,col=col,pch=pch,ncol=2,bg='white'))
+    
+    
+    if(save){
+      graphics.off()
+    }
+    
     return(NULL)
+  }# end compare function
+  
+  ## for debugging
+  ##ddply(Data,~FinalRun,compare,save=FALSE)
+  
+  ddply(Data,~FinalRun,compare,save=SAVE)
+  
+  cat('\n')
+  cat('\n')
+  cat('\n')
+  cat('End assignLSCompare function.\n')
+  
+  return(NULL)
 } # end function assignLSCompare
--- passageWithLifeStageAssign.R
+++ passageWithLifeStageAssign.R
@@ -241,106 +241,122 @@ passageWithLifeStageAssign <- function(site, taxon, min.date, max.date, output.f
             }
 
         }
-
-
-        close(progbar)
-    }
-
-#    ans <<- ans
-#    ans <- get("ans")
-
-    cat("Final lifeStage X run estimates:\n")
-    print(ans)
-
-    #   ---- compute percentages of each life stage
-    ans.pct <- matrix( colSums( ans ), byrow=T, ncol=ncol(ans), nrow=nrow(ans))
-    ans.pct <- ans / ans.pct
-    ans.pct[ is.na(ans.pct) ] <- NA
-
-
-    #   ---- Write out the table
-    df <- data.frame( dimnames(ans)[[1]], ans.pct[,1], ans[,1], lci[,1], uci[,1], stringsAsFactors=F )
-    if( ncol(ans) > 1 ){
-        #   We have more than one run
-        for( j in 2:ncol(ans) ){
-            df <- cbind( df, data.frame( ans.pct[,j], ans[,j], lci[,j], uci[,j], stringsAsFactors=F ))
-        }
-    }
-    names(df) <- c("LifeStage", paste( rep(runs, each=4), rep( c(".propOfPassage",".passage",".lower95pctCI", ".upper95pctCI"), length(runs)), sep=""))
-
-
-    #   ---- Append totals to bottom
-    tots <- data.frame( "Total", matrix( colSums(df[,-1]), nrow=1), stringsAsFactors=F)
-    names(tots) <- names(df)
-    tots[,grep("lower.95", names(tots),fixed=T)] <- NA
-    tots[,grep("upper.95", names(tots),fixed=T)] <- NA
-    df <- rbind( df, Total=tots )
-
-    if( !is.na(output.file) ){
-        out.pass.table <- paste(output.file, "_lifestage_passage_table.csv", sep="")
-        rs <- paste( format(run.season[1], "%d-%b-%Y"), "to", format(run.season[2], "%d-%b-%Y"))
-        nms <- names(df)[1]
-        for( i in 2:length(names(df))) nms <- paste(nms, ",", names(df)[i], sep="")
-
-        cat(paste("Writing passage estimates to", out.pass.table, "\n"))
-
-        sink(out.pass.table)
-        cat(paste("Site=,", catch.df$siteName[1], "\n", sep=""))
-        cat(paste("Site ID=,", catch.df$siteID[1], "\n", sep=""))
-        cat(paste("Species ID=,", taxon, "\n", sep=""))
-        cat(paste("Dates included=,", rs, "\n", sep=""))
-
-        cat("\n")
-        cat(nms)
-        cat("\n")
-        sink()
-
-        write.table( df, file=out.pass.table, sep=",", append=TRUE, row.names=FALSE, col.names=FALSE)
-        out.fn.roots <- c(out.fn.roots, out.pass.table)
-
-        ls.pass.df <<- df
-
-        # Produce pie or bar charts
         
-        # 2/29/2016 -- F.plot.lifestages needs the LifeStage in the rowname.  i have a case where this doesn't happen, where we only
-        # have Parr (so one LifeStage).  I do not know why this hasn't popped up before...
+        #   ---- Update progress bar
+        setWinProgressBar( progbar, getWinProgressBar(progbar)+barinc )
+        out.fn.roots <- c(out.fn.roots, attr(pass, "out.fn.list"))
         
-        rownames(df) <- df$LifeStage
-        fl <- F.plot.lifestages( df, output.file, plot.pies=F )
-        if( fl == "ZEROS" ){
-            cat("FAILURE - F.lifestage.passage - ALL ZEROS\nCheck dates and finalRunId's\n")
-            cat(paste("Working directory:", getwd(), "\n"))
-            cat(paste("R data frames saved in file:", "<none>", "\n\n"))
-            nf <- length(out.fn.roots)
-            cat(paste("Number of files created in working directory = ", nf, "\n"))
-            for(i in 1:length(out.fn.roots)){
-                 cat(paste(out.fn.roots[i], "\n", sep=""))
-            }
-            cat("\n")
-            return(0)
-
-        } else {
-            out.fn.roots <- c(out.fn.roots, fl)
-        }
-
-        #fl <- F.plot.runs( df, output.file, plot.pies=F )
-        #out.fn.roots <- c(out.fn.roots, fl)
+        #print(pass)
+        
+        #   ---- Save
+        ans[ i, j ] <- pass$passage
+        lci[ i, j ] <- pass$lower.95
+        uci[ i, j ] <- pass$upper.95
+        setWinProgressBar( progbar, getWinProgressBar(progbar)+barinc )
+        
+      }
+      
     }
-
-    #   ---- Write out message
-    cat("SUCCESS - F.lifestage.passage\n\n")
-    cat(paste("Working directory:", getwd(), "\n"))
-    cat(paste("R data frames saved in file:", "<none>", "\n\n"))
-    nf <- length(out.fn.roots)
-    cat(paste("Number of files created in working directory = ", nf, "\n"))
-    for(i in 1:length(out.fn.roots)){
-         cat(paste(out.fn.roots[i], "\n", sep=""))
-     }
-     cat("\n")
-
-df
-
-
-
-
+    
+    
+    close(progbar)
+  }
+  
+  #    ans <<- ans
+  #    ans <- get("ans")
+  
+  cat("Final lifeStage X run estimates:\n")
+  print(ans)
+  
+  #   ---- compute percentages of each life stage
+  ans.pct <- matrix( colSums( ans ), byrow=T, ncol=ncol(ans), nrow=nrow(ans))
+  ans.pct <- ans / ans.pct
+  ans.pct[ is.na(ans.pct) ] <- NA
+  
+  
+  #   ---- Write out the table
+  df <- data.frame( dimnames(ans)[[1]], ans.pct[,1], ans[,1], lci[,1], uci[,1], stringsAsFactors=F )
+  if( ncol(ans) > 1 ){
+    #   We have more than one run
+    for( j in 2:ncol(ans) ){
+      df <- cbind( df, data.frame( ans.pct[,j], ans[,j], lci[,j], uci[,j], stringsAsFactors=F ))
+    }
+  }
+  names(df) <- c("LifeStage", paste( rep(runs, each=4), rep( c(".propOfPassage",".passage",".lower95pctCI", ".upper95pctCI"), length(runs)), sep=""))
+  
+  
+  #   ---- Append totals to bottom
+  tots <- data.frame( "Total", matrix( colSums(df[,-1]), nrow=1), stringsAsFactors=F)
+  names(tots) <- names(df)
+  tots[,grep("lower.95", names(tots),fixed=T)] <- NA
+  tots[,grep("upper.95", names(tots),fixed=T)] <- NA
+  df <- rbind( df, Total=tots )
+  
+  if( !is.na(output.file) ){
+    out.pass.table <- paste(output.file, "_lifestage_passage_table.csv", sep="")
+    rs <- paste( format(run.season[1], "%d-%b-%Y"), "to", format(run.season[2], "%d-%b-%Y"))
+    nms <- names(df)[1]
+    for( i in 2:length(names(df))) nms <- paste(nms, ",", names(df)[i], sep="")
+    
+    cat(paste("Writing passage estimates to", out.pass.table, "\n"))
+    
+    sink(out.pass.table)
+    cat(paste("Site=,", catch.df$siteName[1], "\n", sep=""))
+    cat(paste("Site ID=,", catch.df$siteID[1], "\n", sep=""))
+    cat(paste("Species ID=,", taxon, "\n", sep=""))
+    cat(paste("Dates included=,", rs, "\n", sep=""))
+    
+    cat("\n")
+    cat(nms)
+    cat("\n")
+    sink()
+    
+    write.table( df, file=out.pass.table, sep=",", append=TRUE, row.names=FALSE, col.names=FALSE)
+    out.fn.roots <- c(out.fn.roots, out.pass.table)
+    
+    ls.pass.df <<- df
+    
+    # Produce pie or bar charts
+    
+    # 2/29/2016 -- F.plot.lifestages needs the LifeStage in the rowname.  i have a case where this doesn't happen, where we only
+    # have Parr (so one LifeStage).  I do not know why this hasn't popped up before...
+    
+    rownames(df) <- df$LifeStage
+    fl <- F.plot.lifestages( df, output.file, plot.pies=F )
+    if( fl == "ZEROS" ){
+      cat("FAILURE - F.lifestage.passage - ALL ZEROS\nCheck dates and finalRunId's\n")
+      cat(paste("Working directory:", getwd(), "\n"))
+      cat(paste("R data frames saved in file:", "<none>", "\n\n"))
+      nf <- length(out.fn.roots)
+      cat(paste("Number of files created in working directory = ", nf, "\n"))
+      for(i in 1:length(out.fn.roots)){
+        cat(paste(out.fn.roots[i], "\n", sep=""))
+      }
+      cat("\n")
+      return(0)
+      
+    } else {
+      out.fn.roots <- c(out.fn.roots, fl)
+    }
+    
+    #fl <- F.plot.runs( df, output.file, plot.pies=F )
+    #out.fn.roots <- c(out.fn.roots, fl)
+  }
+  
+  #   ---- Write out message
+  cat("SUCCESS - F.lifestage.passage\n\n")
+  cat(paste("Working directory:", getwd(), "\n"))
+  cat(paste("R data frames saved in file:", "<none>", "\n\n"))
+  nf <- length(out.fn.roots)
+  cat(paste("Number of files created in working directory = ", nf, "\n"))
+  for(i in 1:length(out.fn.roots)){
+    cat(paste(out.fn.roots[i], "\n", sep=""))
+  }
+  cat("\n")
+  
+  df
+  
+  
+  
+  
 }
