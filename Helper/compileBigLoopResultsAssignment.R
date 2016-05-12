b39788f9af04212440cd7d93d9e2197807801c40
Jason <jmitchell@west-inc.com>
2016-05-12 08:25:02 -0600

    Final compileBigLooper utilized to construct Excel for Assignment checking.

--- Helper/compileBigLoopResultsAssignment.R
+++ Helper/compileBigLoopResultsAssignment.R
@@ -104,12 +104,15 @@ getRiverPassage <- function(thePlatform,theRiver,stemB){
     test <- test[!is.na(test)]
     rows <- test[which(test=="nrow(expDat):")+1][c(TRUE,FALSE)]
     runs <- test[which(test=="nrow(expDat):")-15][c(TRUE,FALSE)]
+    
+    runs <- ifelse(runs == "fall","Late fall",runs)   # fix for Late fall, which is split into two entries.
+    
     file <- left(right(ls_sinks[i],6),2)
-    by <- "All"
+    #by <- "All"
     
-    runBL <- unlist(strsplit(ls_sinks[1],"--",fixed=TRUE))[c(TRUE,FALSE)]
+    runBL <- unlist(strsplit(ls_sinks[i],"--",fixed=TRUE))[c(TRUE,FALSE)]
     
-    thisData <- data.frame(testi=runBL,file=file,by=by,run=runs,rows=rows)
+    thisData <- data.frame(testi=runBL,file=file,run=runs,rows=rows)
     allData <- rbind(allData,thisData)
   }
   
@@ -130,9 +133,9 @@ ame <- getRiverPassage(thePlatform,'American River',paste0('//lar-file-srv/Data/
 fea <- getRiverPassage(thePlatform,'Feather River',paste0('//lar-file-srv/Data/PSMFC_CampRST/ThePlatform/',thePlatform,'/Outputs'))
 mok <- getRiverPassage(thePlatform,'Mokelumne River',paste0('//lar-file-srv/Data/PSMFC_CampRST/ThePlatform/',thePlatform,'/Outputs'))
 sta <- getRiverPassage(thePlatform,'Stanislaus River',paste0('//lar-file-srv/Data/PSMFC_CampRST/ThePlatform/',thePlatform,'/Outputs'))
-kni <- getRiverPassage(thePlatform,"Knight's Landing",paste0('//lar-file-srv/Data/PSMFC_CampRST/ThePlatform/',thePlatform,'/Outputs'))
+#kni <- getRiverPassage(thePlatform,"Knight's Landing",paste0('//lar-file-srv/Data/PSMFC_CampRST/ThePlatform/',thePlatform,'/Outputs'))
 
-all <- rbind(sac,ame,fea,mok,sta),kni)
+all <- rbind(sac,ame,fea,mok,sta)#,kni)
 nrow(all)
 all <- all[!is.na(all$bEst),]
 all <- all[all$bEst > 0,] 
@@ -154,30 +157,31 @@ options(scipen=999)
 all$bEst <- format(round(as.numeric(all$bEst), 0),nsmall=0,big.mark=",")#,scientific=FALSE) 
 all$bLCL <- format(round(as.numeric(all$bLCL), 0),nsmall=0,big.mark=",")#,scientific=FALSE) 
 all$bUCL <- format(round(as.numeric(all$bUCL), 0),nsmall=0,big.mark=",")
+all$rows <- format(round(as.numeric(levels(all$rows))[all$rows], 0),nsmall=0,big.mark=",")
 
 write.csv(all,"C:/Users/jmitchell/Desktop/allEstsCompare.csv")
 
 
+#as.numeric(levels(f))[f]
 
-
-sac <- getRiverPassage('CAMP_RST20160201','Sacramento River','//lar-file-srv/Data/PSMFC_CampRST/ThePlatform/CAMP_RST20160201/Outputs')
-ame <- getRiverPassage('CAMP_RST20160201','American River','//lar-file-srv/Data/PSMFC_CampRST/ThePlatform/CAMP_RST20160201/Outputs')
-fea <- getRiverPassage('CAMP_RST20160201','Feather River','//lar-file-srv/Data/PSMFC_CampRST/ThePlatform/CAMP_RST20160201/Outputs')
-mok <- getRiverPassage('CAMP_RST20160201','Mokelumne River','//lar-file-srv/Data/PSMFC_CampRST/ThePlatform/CAMP_RST20160201/Outputs')
-sta <- getRiverPassage('CAMP_RST20160201','Stanislaus River','//lar-file-srv/Data/PSMFC_CampRST/ThePlatform/CAMP_RST20160201/Outputs')
-kni <- getRiverPassage('CAMP_RST20160201',"Knight's Landing",'//lar-file-srv/Data/PSMFC_CampRST/ThePlatform/CAMP_RST20160201/Outputs')
-
-allBefore <- rbind(sac,ame,fea,mok,sta,kni)
-nrow(allBefore)
-allBefore <- allBefore[!is.na(allBefore$bEst),]
-allBefore <- allBefore[allBefore$bEst > 0,] 
-rownames(allBefore) <- NULL
-nrow(allBefore)
-allBefore$time <- ifelse(is.na(allBefore$time),'--',allBefore$time)
-allBefore$lifeStage <- as.character(droplevels(allBefore$lifeStage))
-allBefore$lifeStage <- ifelse(is.na(allBefore$lifeStage),'--',allBefore$lifeStage)
-
-compare <- merge(all,allBefore,by=c("by","river","siteName","min.date","max.date","file","run","lifeStage","time"),all.x=TRUE,all.y=TRUE)
-
-
-write.csv(compare,"//lar-file-srv/Data/PSMFC_CampRST/ThePlatform/CAMP_RST20160201/R-Interface/allEstsCompare.csv")
+# sac <- getRiverPassage('CAMP_RST20160201','Sacramento River','//lar-file-srv/Data/PSMFC_CampRST/ThePlatform/CAMP_RST20160201/Outputs')
+# ame <- getRiverPassage('CAMP_RST20160201','American River','//lar-file-srv/Data/PSMFC_CampRST/ThePlatform/CAMP_RST20160201/Outputs')
+# fea <- getRiverPassage('CAMP_RST20160201','Feather River','//lar-file-srv/Data/PSMFC_CampRST/ThePlatform/CAMP_RST20160201/Outputs')
+# mok <- getRiverPassage('CAMP_RST20160201','Mokelumne River','//lar-file-srv/Data/PSMFC_CampRST/ThePlatform/CAMP_RST20160201/Outputs')
+# sta <- getRiverPassage('CAMP_RST20160201','Stanislaus River','//lar-file-srv/Data/PSMFC_CampRST/ThePlatform/CAMP_RST20160201/Outputs')
+# kni <- getRiverPassage('CAMP_RST20160201',"Knight's Landing",'//lar-file-srv/Data/PSMFC_CampRST/ThePlatform/CAMP_RST20160201/Outputs')
+# 
+# allBefore <- rbind(sac,ame,fea,mok,sta,kni)
+# nrow(allBefore)
+# allBefore <- allBefore[!is.na(allBefore$bEst),]
+# allBefore <- allBefore[allBefore$bEst > 0,] 
+# rownames(allBefore) <- NULL
+# nrow(allBefore)
+# allBefore$time <- ifelse(is.na(allBefore$time),'--',allBefore$time)
+# allBefore$lifeStage <- as.character(droplevels(allBefore$lifeStage))
+# allBefore$lifeStage <- ifelse(is.na(allBefore$lifeStage),'--',allBefore$lifeStage)
+# 
+# compare <- merge(all,allBefore,by=c("by","river","siteName","min.date","max.date","file","run","lifeStage","time"),all.x=TRUE,all.y=TRUE)
+# 
+# 
+# write.csv(compare,"//lar-file-srv/Data/PSMFC_CampRST/ThePlatform/CAMP_RST20160201/R-Interface/allEstsCompare.csv")
