0e1d3d525d7ce93b6d07251d90c8469c47221b8f
Jason <jmitchell@west-inc.com>
2016-06-02 15:00:07 -0600

    F.lifestage.passage.assignLS3groupNoWeight.R

--- campR/R/F.lifestage.passage.assignLS3groupNoWeight.R
+++ campR/R/F.lifestage.passage.assignLS3groupNoWeight.R
@@ -4,7 +4,7 @@
 #' 
 #' @description
 #' 
-#'    ANNUAL PRODUCTION ESTIMATES BY LIFE STAGE AND RUN – TABULAR SUMMARY
+#'    ANNUAL PRODUCTION ESTIMATES BY LIFE STAGE AND RUN ? TABULAR SUMMARY
 #'    A table of passage estimates, with lifestages down the rows, and runs across the columns.
 #' 
 #'    Input:
@@ -38,17 +38,18 @@
 #' <insert examples>
 #' 
 F.lifestage.passage.assignLS3groupNoWeight <- function(site, taxon, min.date, max.date, output.file, ci=TRUE){
-###
-###   ANNUAL PRODUCTION ESTIMATES BY LIFE STAGE AND RUN – TABULAR SUMMARY
-###   A table of passage estimates, with lifestages down the rows, and runs across the columns.
-###
-###   Input:
-###   site = site ID of the place we want, trap locaton
-###   taxon = taxon number (from luTaxon) to retrieve
-###
-
-
-    ## 3 life stage groups are fit and weight will not be used to assign lifestage
-    passageWithLifeStageAssign(site=site,taxon=taxon,min.date=min.date,max.date=max.date,output.file=output.file,ci=ci,nLS=3,weightUse=FALSE)
-
+  ###
+  ###   ANNUAL PRODUCTION ESTIMATES BY LIFE STAGE AND RUN Â– TABULAR SUMMARY
+  ###   A table of passage estimates, with lifestages down the rows, and runs across the columns.
+  ###
+  ###   Input:
+  ###   site = site ID of the place we want, trap locaton
+  ###   taxon = taxon number (from luTaxon) to retrieve
+  ###
+  
+  
+  ## 3 life stage groups are fit and weight will not be used to assign lifestage
+  passageWithLifeStageAssign(site=site,taxon=taxon,min.date=min.date,max.date=max.date,output.file=output.file,ci=ci,nLS=3,weightUse=FALSE)
+  
 }
+
