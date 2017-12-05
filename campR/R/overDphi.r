b29f755a76d266a299cbed6e3042dd6b15fb8637
Jason <jmitchell@west-inc.com>
2017-12-05 08:42:00 -0700

    Add in poisson possibility to overDphi function, so as to use the same funciton for catch in eff bootstrapping.

--- campR/R/overDphi.r
+++ campR/R/overDphi.r
@@ -17,8 +17,26 @@ overDphi <- function(model,family="binomial",type){
     if( disp < 1.0 ){
       disp <- 1.0
     }
-  } #else if(family == "poisson"){   # Not currently coded inside this function.
-  #  
-  #}
+  } else if(family == "poisson"){   # Not currently coded inside this function.
+    
+    #   ---- We must cut down the over-dispersion parameter to avoid obvious
+    #   ---- a-typical residuals.  My approach is to toss the largest and smallest
+    #   ---- 20% of residuals, then compute overdispersion.
+    resids <- residuals(c.fit, type)
+    qrds <- quantile( resids, p=c(.2, .8))
+    toss.ind <- (resids < qrds[1]) | (qrds[2] < resids)
+    resids <- resids[!toss.ind]
+    disp <- sum( resids*resids ) / (c.fit$df.residual - sum(toss.ind))
+    if( disp < 1.0 ){
+      disp <- 1.0
+    }
+    
+    #   ---- Uncomment the following line to include all residuals in overdispersion.
+    # disp <- sum(residuals(c.fit, type="pearson")^2) / c.fit$df.residual
+  }
+  
+  #   ---- Visually examine the residuals.  
+  # plot(predict(fit,type="response"), residuals(fit, type))
+    
   return(disp)
 }
