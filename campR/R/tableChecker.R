283b14f16e4d8629030079f838fab3d3d59b222e
Jason <jmitchell@west-inc.com>
2018-05-22 14:15:23 -0600

    New functions tableChecker and tryEnvCovDB.

---
+++ campR/R/tableChecker.R
@@ -0,0 +1,83 @@
+#' @export
+#' 
+#' @title tableChecker
+#' 
+#' @description Query the Environmental Covariate Database for all internal
+#'   tables, and in the case any identified "temp" tables are missing, insert
+#'   "fake" ones, so as to ensure those temp tables are available for deletion
+#'   in subsequent SQL queries.
+#'   
+#' @details The function requires no arguments.  The location of the CAMP.mdb of
+#'   interest is known via externally defined global variable \code{db.file}.
+#'   
+#'   Additionally, the current set of temp tables used for checking is
+#'   hard-coded within the function, so this set of tables does not require
+#'   specification.
+#'   
+#' @return The function is silent.  It simply reports messages to the log
+#'   file indicating connection attempts.  In the case a connection is
+#'   successful, a message of success is recorded.
+#'   
+#' @author WEST, Inc.
+#'   
+function <- tableChecker(){
+  
+  #   ---- See what tables we have in our current db.file, or CAMP.mdb.  
+  db <- get( "db.file", envir=.GlobalEnv )
+  ch <- odbcConnectAccess(db)
+  tables <- RODBC::sqlTables(ch)
+  tables <- tables[tables$TABLE_TYPE == "TABLE",]$TABLE_NAME
+  close(ch)
+  
+  #   ---- Define the set of all tables.  This should perhaps be in GlobalVars.  
+  datTables <- c("CatchRaw","CustomQueries","Dates","EnvDataDaily",                     
+                 "EnvDataDailyXTargetSite","EnvDataRaw",       
+                 "EnvDataRawXTargetSite","LengthAtDate",
+                 "MarkApplied","MarkExisting","MarkShortcuts","People",                           
+                 "PostHandleMort","PostHandleMortMark","ProjectDescription","ReadMe",                           
+                 "Release","ReleaseFish","Settings",                         
+                 "Site","Specimen","StreamFlow","SubSite",                          
+                 "TaxonImages","TaxonNative","TrapVisit","Version")
+  luTables  <- c("luActive","luAgency",                       
+                 "luBodyPart","luColor","luConeDebrisVolumeCat","luDebrisVolumeCat",                
+                 "luFishOrigin","luFishProcessed","luLifeStage","luLifeStageCAMP",                  
+                 "luLightCondition","luMarkType","luNoYes","luQueryType",                      
+                 "luReleasePurpose","luRights","luRun","luRunMethod",                      
+                 "luSampleGear","luSpecimenType","luSubsampleMethod","luTaxon",                          
+                 "luTrapFunctioning","luUnit","luVisitType")
+  tmpTables <- c("EnvDataRaw_Standardized","EnvDataRaw_StandardSelect","NonTrapSample1","ReleaseXTargetSite","TrapSample1",
+                 "TempChinookSampling_a","TempChinookSampling_b",            
+                 "TempChinookSampling_c","TempChinookSampling_d1","TempChinookSampling_d2","TempChinookSampling_d3",           
+                 "TempChinookSampling_e","TempChinookSampling_f","TempChinookSampling_g","TempChinookSampling_h",            
+                 "TempChinookSampling_i_final","TempEffortSummary_a","TempEffortSummary_b","TempNonSamplingSummary",           
+                 "TempRelRecap_1","TempRelRecap_3","TempRelRecap_final","TempReportCriteria_Release",       
+                 "TempReportCriteria_Trapvisit","tempSamplingSumCHN","TempSamplingSummary","TempSamplingSummary_Final",        
+                 "TempSumUnmarkedByTrap_Run_a","TempSumUnmarkedByTrap_Run_b","TempSumUnmarkedByTrap_Run_Final","TempSumUnmarkedByTrap_Run_Final2", 
+                 "TempSumUnmarkedByTrap_Run_X_Final")
+  
+  #   ---- Check to make sure tables in queried mdb in allTables check vector.  
+  if(any(!(tmpTables %in% tables))){
+    
+    #   ---- We are missing a tmp table somehow.  Make a copy of a table that exists.  Only copy a Temp table, although
+    #   ---- I guess any table could be used, and we could probably just use a CREATE TABLE type of statement; i.e.,
+    #   ---- not use an existing table at all.  
+    for(i in 1:sum(!(tmpTables %in% tables))){
+      
+      #   ---- Go through all the missing tables and make a copy, one-by-one.  
+      needThisTmpTable <- tmpTables[!(tmpTables %in% tables)][i]
+
+      #   ---- Make a fake dataframe to push up to CAMP.mdb.  
+      fake <- data.frame(Fake=seq(1,10,1))
+      
+      #   ---- Connect and push the fake dataframe.  
+      ch <- odbcConnectAccess(db)
+      sqlSave(ch,
+              fake,
+              tablename=needThisTmpTable,
+              varTypes=c(Fake="integer"),
+              rownames=FALSE)
+      cat(paste0("I copied in a version of temp table ",needThisTmpTable,".\n"))
+      close(ch)
+    }
+  }
+}
---
+++ campR/R/tryEnvCovDB.R
@@ -0,0 +1,61 @@
+#' @export
+#' 
+#' @title tryEnvCovDB
+#'   
+#' @description Query the Environmental Covariate Database for other users prior
+#'   to creating internal database tables.
+#'   
+#' @param nTries The number of attempts to query the Environmental Covariate
+#'   Database.
+#'   
+#' @param secSleep The number of seconds to wait between attempts.
+#'   
+#' @details The function is a simple \code{repeat}-type loop, running at most
+#'   \code{nTries} times.
+#'   
+#' @return The function is nearly silent.  It simply reports messages to the log
+#'   file indicating connection attempts.  In the case a connection is
+#'   successful, a message of success is recorded.
+#'   
+#' @author WEST, Inc.
+#'   
+tryEnvCovDB <- function(nTries,secSleep){
+
+  # nTries <- 24
+  # secSleep <- 5
+
+  checkForOthers <- RPostgres::dbSendQuery(ch,"SELECT COUNT(usename) FROM pg_stat_activity WHERE usename = 'envcovread';")
+  nOthers <- RPostgres::dbFetch(checkForOthers) - 1
+  RPostgres::dbClearResult(checkForOthers)
+
+  if(nOthers == 0){
+    cat(paste("You are the only 'envcovread'.  Proceed to query.\n"))
+  } else if(nOthers >= 1){
+    try <- 0
+    repeat{  
+      
+      #   ---- We try 24 times, or 120 seconds = 2 minutes.  
+      try <- try + 1
+      if(try <= nTries){
+        if(nOthers == 0){
+          cat(paste0("You are now the only 'envcovread'.  Proceed to query.\n"))
+          break
+        } else if(nOthers == 1){
+          cat(paste0("Another 'envcovread' is signed in.  This is try number ",try,". I will wait 5 seconds and check again.\n"))
+        } else if(nOthers >= 2){
+          cat(paste0("At least two other 'envcovread's are signed in.  This is try number ",try,".  I will wait 5 second and try again.\n"))
+        }
+        
+        #   ---- See if we can get in now.
+        Sys.sleep(secSleep)
+        checkForOthers <- RPostgres::dbSendQuery(ch,"SELECT COUNT(usename) FROM pg_stat_activity WHERE usename = 'envcovread';")
+        nOthers <- RPostgres::dbFetch(checkForOthers) - 1
+        RPostgres::dbClearResult(checkForOthers)
+        
+      } else {
+        cat(paste0("I cannot get into the Environmental Covariate Database after 24 attempts.  Contact the CAMP Administrator or try again later.\n"))
+        stop()
+      }
+    }
+  } 
+}
