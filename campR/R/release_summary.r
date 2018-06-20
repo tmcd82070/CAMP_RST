23aa5909786489705dc76f71486815615e08551f
Jason <jmitchell@west-inc.com>
2018-06-20 14:39:56 -0600

    Update release_summary to handle case when there are no releases.

--- campR/R/release_summary.r
+++ campR/R/release_summary.r
@@ -63,48 +63,62 @@ F.release.summary <- function(site,taxon,run,min.date,max.date,output.file){
   #   ---- Fetch efficiency data.
   release.df <- F.get.release.data.light( site, taxon, min.date, max.date )
 
-  #   ---- Summarize the releases.
-  release.sum <- F.summarize.releases( release.df )
-
   #   ---- Get a couple of variables from quickie lookups.
   db <- get( "db.file", envir=.GlobalEnv ) 
-  ch <- odbcConnectAccess(db)
-  siteTable <- sqlFetch( ch, "site" )[,c('siteName','siteAbbreviation','siteID')]
-  runTable <- sqlFetch( ch, "luRun" )   
+  ch <- RODBC::odbcConnectAccess(db)
+  siteTable <- RODBC::sqlFetch( ch, "site" )[,c('siteName','siteAbbreviation','siteID')]
+  runTable <- RODBC::sqlFetch( ch, "luRun" )   
   close(ch) 
-    
-  #   ---- Write summary to CSV file.
-  out.fn <- paste(output.file, "_release_summary.csv", sep="")
-  rs <- paste( format(as.POSIXlt(min.date,tz=""), "%d-%b-%Y"), "to", format(as.POSIXlt(max.date,tz=""), "%d-%b-%Y"))
-  nms <- names(release.sum)[1]
-  for( i in 2:length(names(release.sum))) nms <- paste(nms, ",", names(release.sum)[i], sep="")
-
-  sink(out.fn)
-  cat(paste("Site=,", release.df$siteName[1], "\n", sep=""))
-  cat(paste("Site abbreviation=,", siteTable[siteTable$siteID == site,]$siteAbbreviation, "\n", sep=""))
-  cat(paste("Site ID=,", attr(release.df,"siteID"), "\n", sep=""))
-  if(taxon == 161980){
-    cat(paste("Species=,", "Chinook Salmon", "\n", sep=""))
-  } else if(taxon == 161989){
-    cat(paste("Species=,", "Steelhead / rainbow trout", "\n", sep=""))
-  }
-  cat(paste("Species ID=,", attr(release.df,"taxonID"), "\n", sep=""))
-  cat(paste("Run=,", runTable[runTable$runID == run,]$run, "\n", sep=""))
-  cat(paste("Run ID=,", run, "\n", sep=""))
-  cat(paste("Run season=,", rs, "\n", sep=""))
-  cat("\n")
-  cat(nms)
-  cat("\n")
-  sink()
-  write.table( release.sum, file=out.fn, sep=",", append=TRUE, row.names=FALSE, col.names=FALSE)
   
-  #   ---- Send messages back to the interface.
-  cat("SUCCESS - F.release.summary\n\n")
-  cat(paste("Working directory:", getwd(), "\n"))
-  cat(paste("R data frames saved in file:", "<No RData saved>", "\n\n"))
-  cat("Number of files created in working directory = 1\n")
-  cat(paste(out.fn, "\n"))
-  cat("\n")
+  #   ---- Summarize the releases.
+  if(is.null(release.df)){
+    
+    #   ---- In the case we have nothing, output as such, so the program finishes nicely.  
+    out.fn <- paste(output.file, "_release_summary.csv", sep="")
+    rs <- paste( format(as.POSIXlt(min.date,tz=""), "%d-%b-%Y"), "to", format(as.POSIXlt(max.date,tz=""), "%d-%b-%Y"))
+    utils::write.csv(data.frame(Site=site,
+                                "Site Abbreviation"=siteTable[siteTable$siteID == site,]$siteAbbreviation,
+                                "Run season"=rs,
+                                Run=runTable[runTable$runID == run,]$run,
+                                Releases="No releases found."),
+                     out.fn,row.names=FALSE)
+  } else {
+    
+    release.sum <- F.summarize.releases( release.df )
 
-  invisible(release.sum)
+    #   ---- Write summary to CSV file.
+    out.fn <- paste(output.file, "_release_summary.csv", sep="")
+    rs <- paste( format(as.POSIXlt(min.date,tz=""), "%d-%b-%Y"), "to", format(as.POSIXlt(max.date,tz=""), "%d-%b-%Y"))
+    nms <- names(release.sum)[1]
+    for( i in 2:length(names(release.sum))) nms <- paste(nms, ",", names(release.sum)[i], sep="")
+  
+    sink(out.fn)
+    cat(paste("Site=,", release.df$siteName[1], "\n", sep=""))
+    cat(paste("Site abbreviation=,", siteTable[siteTable$siteID == site,]$siteAbbreviation, "\n", sep=""))
+    cat(paste("Site ID=,", attr(release.df,"siteID"), "\n", sep=""))
+    if(taxon == 161980){
+      cat(paste("Species=,", "Chinook Salmon", "\n", sep=""))
+    } else if(taxon == 161989){
+      cat(paste("Species=,", "Steelhead / rainbow trout", "\n", sep=""))
+    }
+    cat(paste("Species ID=,", attr(release.df,"taxonID"), "\n", sep=""))
+    cat(paste("Run=,", runTable[runTable$runID == run,]$run, "\n", sep=""))
+    cat(paste("Run ID=,", run, "\n", sep=""))
+    cat(paste("Run season=,", rs, "\n", sep=""))
+    cat("\n")
+    cat(nms)
+    cat("\n")
+    sink()
+    utils::write.table( release.sum, file=out.fn, sep=",", append=TRUE, row.names=FALSE, col.names=FALSE)
+  
+    #   ---- Send messages back to the interface.
+    cat("SUCCESS - F.release.summary\n\n")
+    cat(paste("Working directory:", getwd(), "\n"))
+    cat(paste("R data frames saved in file:", "<No RData saved>", "\n\n"))
+    cat("Number of files created in working directory = 1\n")
+    cat(paste(out.fn, "\n"))
+    cat("\n")
+  
+    invisible(release.sum)
+  }
 }
